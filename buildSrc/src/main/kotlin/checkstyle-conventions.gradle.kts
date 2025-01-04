6d981e0f10da8dd0eb093027a108848ae3a2bd9d
Jake Potrebic <jake.m.potrebic@gmail.com>
2025-01-04 14:56:58 -0800

    simplify to just skip all packages until manually removed

--- buildSrc/src/main/kotlin/checkstyle-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/checkstyle-conventions.gradle.kts
@@ -1,7 +1,6 @@
 import io.papermc.paperweight.util.convention
 import java.nio.file.Path
 
-
 abstract class CustomCheckstylePlugin : CheckstylePlugin() {
 
     override fun getTaskType(): Class<Checkstyle> {
@@ -23,28 +22,15 @@ extensions.configure<CheckstyleExtension>() {
     configDirectory = localCheckstyleConfigDir
 }
 
-val gitUserProvider: Provider<String> = if (System.getenv("CI") == "true") {
-    providers.environmentVariable("GIT_USER").map { it.trim() }
-} else {
-    providers.exec { commandLine("git", "config", "--get", "user.name") }.standardOutput.asText.map { it.trim() }
-}
-val changedFilesSource = providers.of(ChangedFilesSource::class) {}
-
-val collectDiffedData = tasks.register<CollectDiffedDataTask>("collectDiffedData") {
-    uncheckedFiles.set(providers.fileContents(localCheckstyleConfigDir.file("unchecked-files.txt")).asText.map { it.split("\n").toSet() })
-    specialUsers.set(providers.fileContents(checkstyleExtraConfigDir.file("users-who-can-update.txt")).asText.map { it.split("\n").toSet() })
-    changedFiles.set(changedFilesSource)
-    gitUser.set(gitUserProvider)
-}
-
 val typeUseAnnotationsProvider: Provider<Set<String>> = providers.fileContents(checkstyleExtraConfigDir.file("type-use-annotations.txt"))
     .asText.map { it.trim().split("\n").toSet() }
 
+val packagesToSkipSource: Provider<Set<String>> = providers.fileContents(localCheckstyleConfigDir.file("packages.txt")).asText.map { it.trim().split("\n").toSet() }
+
 tasks.withType<CustomCheckstyleTask> {
     rootPath = project.rootDir.path
-    changedFilesTxt = collectDiffedData.flatMap { it.changedFilesTxt }
+    packagesToSkip = packagesToSkipSource
     runForAll = providers.gradleProperty("runCheckstyleForAll").map { it.toBoolean() }
-    filesToRemoveFromUncheckedTxt = collectDiffedData.flatMap { it.filesToRemoveFromUncheckedTxt }
     typeUseAnnotations = typeUseAnnotationsProvider
 }
 
